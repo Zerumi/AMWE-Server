814fd51dd347fccc3208fee411aede20b1903d80
Zerumi <evgexa.2015@bk.ru>
2021-04-23 18:56:59 +0400

    AMWE Site part 1

--- AMWE Main Site/Pages/Download.cshtml.cs
+++ AMWE Main Site/Pages/Download.cshtml.cs
@@ -1,18 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AMWE_Main_Site.Pages
 {
-    public class ContactModel : PageModel
+    public class DownloadModel : PageModel
     {
-        public string Message { get; set; }
-
         public void OnGet()
         {
-
         }
     }
 }
